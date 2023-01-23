3406b97f4b1f3dda34b9e7f89c3a2b555ceec8a4
Barbara Hellen <barbarapereira123@hotmail.com>
2023-01-23 17:19:21 -0300

    feat: navbar e inicia carousel home page

--- utils/CarouselHomePage.ts
+++ utils/CarouselHomePage.ts
@@ -1,5 +1,9 @@
 import { TCarouselItems } from "@/typings/CarouselItems";
 
 export const CarouselItems: TCarouselItems[] = [
-    {id: "id-0", img: "https://www.macau.rn.leg.br/images/galeria_em_artigos/image04_grd.png", title: "Titulo Imagem"}
+    {id: "id-0", img: "https://www.macau.rn.leg.br/images/galeria_em_artigos/image04_grd.png", title: "Titulo Imagem"},
+    {id: "id-0", img: "https://www.macau.rn.leg.br/images/galeria_em_artigos/image04_grd.png", title: "Titulo Imagem"},
+    {id: "id-0", img: "https://www.macau.rn.leg.br/images/galeria_em_artigos/image04_grd.png", title: "Titulo Imagem"},
+    {id: "id-0", img: "https://www.macau.rn.leg.br/images/galeria_em_artigos/image04_grd.png", title: "Titulo Imagem"},
+    {id: "id-0", img: "https://www.macau.rn.leg.br/images/galeria_em_artigos/image04_grd.png", title: "Titulo Imagem"},
 ]
