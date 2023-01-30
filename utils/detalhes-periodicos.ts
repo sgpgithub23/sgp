5f018c73a572559ceda56b11fb5ad8a0a7a30623
Barbara Hellen <barbarapereira123@hotmail.com>
2023-01-30 19:30:36 -0300

    feat: adiciona informações de sessão

--- utils/detalhes-periodicos.ts
+++ utils/detalhes-periodicos.ts
@@ -5,23 +5,23 @@ export const Periodicos: {title: string, descricao: string}[] =
             descricao: "Entrevista exclusiva com renomados juristas da atualidade, sobre importantes assuntos relacionados ao tema do respectivo periódico"
         },
         {
-            title: "Seção Soluções Práticas", 
+            title: "Seção de Soluções Práticas", 
             descricao: "Questões práticas na temática do periódico, devidamente fundamentadas em doutrina e jurisprudência atuais, que refletem o atual entendimento do corpo jurídico da Orientação."
         },
         {
-            title: "Seção Soluções Autorais", 
+            title: "Seção de Soluções Autorais", 
             descricao: "Artigos técnico-científicos de ilustres especialistas que integram o nosso rol de colaboradores, sobre temas pertinentes ao respectivo periódico"
         },
         {
-            title: "Seção Soluções Jurisprudenciais", 
+            title: "Soluções Jurisprudenciais", 
             descricao: "Julgados atuais e relevantes das Cortes de Contas e Tribunais Superiores, criteriosamente selecionados de acordo com a pertinência de cada periódico."
         },
         {
-            title: "Seção Saindo das Licitações e Contratos", 
+            title: "Licitações e Contratos", 
             descricao: "Artigos técnico-científicos de ilustres especialistas que integram o nosso rol de colaboradores, com temática que refoge as licitações e contratos"
         },
         {
-            title: "Seção Soluções em Propostas Legislativas", 
+            title: "Propostas Legislativas", 
             descricao: "Análises pontuais quanto à constitucionalidade de projetos de leis, verificando principalmente competência e iniciativa"
         },
 
