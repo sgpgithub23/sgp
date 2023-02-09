67dd1af14c7da0b118bf93d4aeca4814ccd75dfe
Barbara Hellen <barbarapereira123@hotmail.com>
2023-02-08 23:25:58 -0300

    feat: documentacoes

---
+++ utils/certidoes-declaracoes.ts
@@ -0,0 +1,9 @@
+export const CertidoesDeclaracoes: {titulo: string, link: string}[] = [
+    {link: "", titulo: "Declaracao do Sindicato das Entidades Culturais - (Atestado de Exclusividade)"},
+    {link: "", titulo: "Certidão Conjunta de Débitos de Tributos Mobiliários"},
+    {link: "", titulo: "Certidão Negativa de Débitos Trabalhistas"},
+    {link: "", titulo: "Certidão Estadual de Dívida Ativa do Estado de São Paulo"},
+    {link: "", titulo: "Certidão Negativa de Débitos relativos aos Tributos Federais e a Divida Ativa da União"},
+    {link: "", titulo: "Certificado de Regularidade do FGTS - CRF - (CND FGTS)"},
+    {link: "", titulo: "Certidão de Falência, Concordatas, Recuperações Judiciais e Extrajudiciais"},
+]
