add9d3017866183f4c3d531dc5337127ed4743e1
Barbara Hellen <barbarapereira123@hotmail.com>
2023-02-12 17:41:18 -0300

    feat: add tipo orientacoes

---
+++ utils/tipos-orientacoes.ts
@@ -0,0 +1,37 @@
+export const TiposOrientacoes: string[][] = [
+    [
+        "Concessões e permissões de serviços públicos",
+        "Licitações e contratos administrativos",
+        "Parcerias Público-Privadas",
+        "Convênios, consórcios e parcerias",
+        "Atos administrativos",
+        "Autarquias, fundações, empresas públicas e sociedades de economia mista",
+
+    ],
+    [
+        "Terceiro setor",
+        "Sistema S",
+        "Agentes públicos",
+        "Processo administrativo disciplinar e sindicância",
+        "Poder de polícia",
+        "Infrações e sanções administrativas",
+        "Bens públicos",
+    ], 
+    [
+        "Desapropriação",
+        "Servidão administrativa",
+        "Controle da Administração",
+        "Improbidade administrativa",
+        "Responsabilidade civil do Estado",
+        "Responsabilidade fiscal",
+        "Estatuto da Cidade",
+        "Parcelamento do Solo",
+    ],
+    [
+        "Impostos, taxas e contribuições (no âmbito municipal)",
+        "Prefeitos e Vereadores",
+        "Análise de projetos de lei quanto à iniciativa e competência",
+        "Direito Eleitoral (afeto aos Poderes Legislativo e Executivo)",
+        "Direito Previdenciário (afeto aos agentes públicos)",
+    ],
+]
