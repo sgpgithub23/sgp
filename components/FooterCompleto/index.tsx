070ec7ee05803e49fb4c361ef031d3a29e0789f4
Barbara Hellen <barbara.pereira@modalgr.com.br>
2023-06-12 10:40:48 -0300

    feat: altera mapa footer

--- components/FooterCompleto/index.tsx
+++ components/FooterCompleto/index.tsx
@@ -116,8 +116,9 @@ export function FooterCompleto() {
           </div>
           <div className={styles.iframeSection}>
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d3657.638149853922!2d-46.64347088538432!3d-23.545513666891484!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1srua%20sete%20de%20abril%2C%20252%2012o.%20andar%20conjunto%20120%20e%20121!5e0!3m2!1spt-BR!2sbr!4v1665499844026!5m2!1spt-BR!2sbr"
-              allowFullScreen
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1828.8188347353162!2d-46.64261956112765!3d-23.545530935805044!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce5851fe63efbb%3A0xdb555a03e320e2b!2sCondom%C3%ADnio%20Edif%C3%ADcio%20Alois!5e0!3m2!1spt-BR!2sbr!4v1686063672901!5m2!1spt-BR!2sbr"
+              style={{ border: 0 }}
+              allowFullScreen={false}
               loading="lazy"
               referrerPolicy="no-referrer-when-downgrade"
             ></iframe>
